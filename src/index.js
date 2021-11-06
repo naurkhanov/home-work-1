5de031e0c5bef15d643545f938d75cdbd8632354
Deni Naurkhanov <naurkhanovdeni87@gmail.com>
2021-11-06 16:08:18 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './tutorial-5/App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './index.css'
 // import App from './tutorial-1/App';
 // import App from './tutorial -2/App';
 // import App from "./tutorial-3/App";
 // import App from './tutorial-4/App';
+// import App from './tutorial-5/App';
+
+import App from './tutorial-6/App';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/tutorial-6/App.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Col, Nav, Navbar, Row, Card } from 'react-bootstrap';
+import Article from './components/Article';
+
+const App = () => {
+  const { pathname } = window.location;
+  const arr = window.location.pathname.split('/');
+  const secondParam = arr[2];
+
+  console.log(window.location);
+  return (
+    <div>
+      <header>
+        <h2>
+          <a href="/">React Blog</a>
+        </h2>
+        <Nav variant="pills" defaultActiveKey="/">
+          <Nav.Item>
+            <Nav.Link eventKey="/home" to="/">
+              Главная
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="/home" to="/about">
+              Обо мне
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="/home" to="/profile">
+              Профиль
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </header>
+      {pathname === '/' && (
+        <Row xs={1} md={3} className="g-4">
+          <Col>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://via.placeholder.com/150x150"
+              />
+              <Card.Body>
+                <Card.Title>
+                  <a href="/post/1">Card title</a>
+                </Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      )}
+      {pathname === `/post/${secondParam}` && <Article id={secondParam} />}
+      {pathname === '/about' && (
+        <Card>
+          <Card.Body>Это мой личный сайт!</Card.Body>
+        </Card>
+      )}
+      <br />
+      <Navbar bg="light" style={{ paddingLeft: 20 }}>
+        <Navbar.Brand href="#home">My site (c) 2021</Navbar.Brand>
+      </Navbar>
+    </div>
+  );
+};
+
+export default App;
