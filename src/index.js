e0dedd64f27110139f0c1bf52a4d7ba79b5950d1
Deni Naurkhanov <naurkhanovdeni87@gmail.com>
2021-11-15 15:58:06 +0300

    ref

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 // import App from './tutorial-1/App';
-// import App from './tutorial -2/App';
+// import App from './tutorial-2/App';
 // import App from "./tutorial-3/App";
 // import App from './tutorial-4/App';
 // import App from './tutorial-5/App';
-
-import App from './tutorial-6/App';
+// import App from './tutorial-6/App';
+import App from './tutorial-7/App';
 
 ReactDOM.render(
   <React.StrictMode>
